12d6a9ebe2212dca7a14e347431bc9cd6d046e4e
iisti <juho.ita@gmail.com>
2022-04-25 19:57:57 +0300

    changed that all repos can be dumped automatically, without defining them separately

--- create_svn_dumps.bash
+++ create_svn_dumps.bash
@@ -2,13 +2,29 @@
 
 # Script for creating SVN dumps of repos
 
-arr_repos=()
-arr_repos+=(reponame1)
-arr_repos+=(reponame2)
-arr_repos+=(reponame3)
-
 repo_path="/opt/storage/svn_repos/"
 dump_path="/opt/storage/svn_dumps/"
+
+# Define repos separately or load all repos from certain path.
+arr_repos=()
+
+# Add repos separately if required
+#arr_repos+=(reponame1)
+#arr_repos+=(reponame2)
+#arr_repos+=(reponame3)
+
+# Or extract repo names from repo_path. Uncomment "repos_multiline" and "mapfile"
+
+# Explanation of command:
+#   find only depth 1 directories
+#   reverse paths, so that the last dir (=repo) is the first
+#   cut with / as delimiter and select 1st field (=repo dir)
+#   reverse again, so that repo names are correct
+# The repos are saves as multiline string.
+#repos_multiline=$(find $repo_path -mindepth 1 -maxdepth 1 -type d | rev | cut -d'/' -f 1 | rev)
+
+# Convert multiline string to array
+#mapfile -t arr_repos <<< "$repos_multiline"
 
 datefile=$(date +"%Y-%m-%d_%H-%M")
 
